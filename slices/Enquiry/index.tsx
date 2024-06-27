271b409fda506716c254d42574f792cfcf9e07ed
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-28 00:27:07 +0100

    [styling]:enquiry section

--- slices/Enquiry/index.tsx
+++ slices/Enquiry/index.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import Bounded from '../../app/components/Bounded';
+import { useRef } from "react";
+import Image from "next/image";
+import oilrig from "../../public/oilrig.png"
 
 /**
  * Props for `Enquiry`.
@@ -10,13 +16,54 @@ export type EnquiryProps = SliceComponentProps<Content.EnquirySlice>;
  * Component for "Enquiry" Slices.
  */
 const Enquiry = ({ slice }: EnquiryProps): JSX.Element => {
+
+  const headerref = useRef(null)
+  const button = useRef(null)
+
+
+
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
+    <Bounded
+    // data-slice-type={slice.slice_type}
+    // data-slice-variation={slice.variation}
+   className="  text-[#333D3E] bg-[#EDF4F6]" 
     >
-   
-    </section>
+<div className="content w-full flex landscape:justify-between portrait:flex-col portrait:items-center pb-[6vw] pt-10 space-y-[4vw] portrait:space-y-[12vw]"> 
+
+  <div className="form w-[50%] portrait:w-full">
+
+  <div className="clients_writeup_lead  mb-4 portrait:mt-10  portrait:mb-10 mt-4 text-[2vw]  portrait:text-[6vw] portrait:sm:text-[3vw]">{slice.primary.writeup}</div>
+
+<form action="" className="flex flex-col items-center">
+
+<div className="inputs w-full grid  portrait:grid-cols-1 landscape:grid-cols-2  gap-5 portrait:gap-[6vw] portrait:sm:gap-y-[6vw]   gap-y-6">
+<input required type="text" className='brandname outline-none h-[3vw] portrait:h-[14vw] px-3 text-[#EDF4F6] bg-[#2F3D47] rounded-md' name="brandname" placeholder='Add a Brand Name'  />
+<input required type="text" className='title outline-none h-[3vw] portrait:h-[14vw] px-3 text-[#EDF4F6] bg-[#2F3D47] rounded-md' name="title" placeholder='Add a Title' />
+<input required type="text" className='shortdescription outline-none h-[3vw] portrait:h-[14vw] px-3 text-[#EDF4F6] bg-[#2F3D47] rounded-md' name="shortdescription" placeholder='Add a Short Description' />
+<input required type="text" className='shortdescription outline-none h-[3vw] portrait:h-[14vw] px-3 text-[#EDF4F6] bg-[#2F3D47] rounded-md' name="shortdescription" placeholder='Add a Short Description' />
+<textarea required  className='fulldescription w-[48.5vw] portrait:w-full outline-none h-[12vw] resize-none portrait:h-[28vw] px-3 py-1 text-[#EDF4F6] bg-[#2F3D47] rounded-md'  name="fulldescription" placeholder='Add a full Description' />
+</div>
+
+<button type="submit" className="btn mt-10  portrait:w-full bg-[#423B17] text-[#EDF4F6] cursor-pointer portrait:px-[22vw] px-[6vw] py-3 rounded-[0.280rem] text-[1.8vw] portrait:text-[8vw] hover:bg-[#252d2e] duration-[0.2s]  ease-in-out ">Submit</button>
+
+
+
+
+
+
+
+
+</form>
+
+
+  </div>
+
+  <div className="svg w-[30%] portrait:w-full">
+    <Image src={oilrig} alt="oilrig_svg" className="object-fit "/>
+  </div>
+
+      </div>
+      </Bounded>
   );
 };
 
