08453b5919ad4ce40f3596605b3d4b975e0156d7
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-28 01:51:28 +0100

    [styling]:footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,13 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
-import Bounded from "./Bounded";
+import Bounded from "./FooterBounded";
 import Image from "next/image";
 import gmailIcon from "../../public/gmailIcon.png";
 import whatsappIcon from "../../public/whatsappIcon.png";
 import instagramIcon from "../../public/instagramIcon.png";
 import facebookIcon from "../../public/facebookIcon.png";
+import footerBG from "../../public/footer_bg.png"
+import formosaIMG from "../../public/FORMOSA.png"
 
 
 
@@ -18,10 +20,36 @@ export default async function Footer() {
 
   return (
     <Bounded>
-    <div className="content  bg-[#141C1C] text-[#c9bfc4]  w-full px-5 py-8 pt-8 flex flex-row justify-between  portrait:sm:flex-row  portrait:sm:justify-between portrait:flex-col portrait:space-y-6 portrait:sm:space-y-0  ">
+    <div className="content relative landscape:h-[150vh]  bg-[#EEF4F8] text-[#282E30]  w-full px-5 py-8 pt-20 flex flex-col space-y-4    portrait:sm:justify-between portrait:flex-col portrait:space-y-6 portrait:sm:space-y-0  ">
 
 
-<div className="addressNpolicySection space-y-3 landscape:w-[40%] portrait:sm:w-[50%]">
+
+  <Image
+    src={footerBG}
+    alt="Background"
+    layout="fill"
+    objectFit="cover"
+    priority
+    className=" w-full h-full  "
+  />
+
+
+
+<div className="details w-[98%]  flex portrait:flex-col portrait:space-y-8 landscape:space-x-[20%] ">
+
+<div className="addressNpolicySection space-y-3 landscape:w-[40%] portrait:sm:w-[100%]">
+
+<div className="addressDiv space-y-1">
+  <div className="title text-[1.7vw] portrait:sm:text-[4vw] portrait:text-[7vw]">About Us</div>
+  <div className="address text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw] ">Who We Are</div>
+  <div className="address text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw]">Mission and Vision</div>
+</div>
+
+
+
+</div>
+
+<div className="addressNpolicySection space-y-3 landscape:w-[40%] portrait:sm:w-[100%]">
 
 <div className="addressDiv space-y-1">
   <div className="title text-[1.7vw] portrait:sm:text-[4vw] portrait:text-[7vw]">Address</div>
@@ -32,10 +60,7 @@ export default async function Footer() {
 
 </div>
 
-
-
-
-<div className="customerSupportSection space-y-3">
+<div className="customerSupportSection w-[40%] portrait:w-full space-y-3">
   <div className="header text-[1.7vw] portrait:sm:text-[4vw] portrait:text-[7vw]">Customer Support</div>
   <div className="refundPolicyLink text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[5vw] hover:text-[#E2CABE]">
     {/* <PrismicNextLink field={settings.data.refundpolicy}>Refund Policy</PrismicNextLink> */}
@@ -53,10 +78,10 @@ export default async function Footer() {
 
 
 <div className="icons flex space-x-6 portrait:space-x-9"> 
-<Image src={ gmailIcon} alt="gmail icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/>
-<Image src={ whatsappIcon} alt="whatsapp icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/>
-<Image src={ instagramIcon} alt="instagram icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/>
-<Image src={ facebookIcon} alt="facebook icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/>
+{/* <Image src={ gmailIcon} alt="gmail icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/> */}
+{/* <Image src={ whatsappIcon} alt="whatsapp icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/> */}
+{/* <Image src={ instagramIcon} alt="instagram icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/> */}
+{/* <Image src={ facebookIcon} alt="facebook icon" className=" w-[2vw] portrait:w-[7vw] portrait:sm:w-[3.5vw] object-contain"/> */}
 
 </div>
 
@@ -69,10 +94,18 @@ export default async function Footer() {
 
 </div>
 
+</div>
+
+
+<div className="formosa text-[20vw] portrait:text-[18vw]">FORMOSA</div>
+
+
+
+
   
     </div>
   
-  <div className="copyRightDiv flex portrait:flex-col justify-between px-5 py-2 text-[#2a3a3a]  text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[3.7vw]  "> <div className="preciousOG">site by PreciousOG</div>   <div className="prodigital">© 2024 Pro Digital - All Rights Reserved</div> </div>
+  <div className="copyRightDiv relative flex portrait:flex-col justify-between px-5 py-2 bg-[#282E30] text-[#EEF4F8]  text-[1.2vw]  portrait:sm:text-[3vw] portrait:text-[3.7vw]  "> <div className="preciousOG">site by PreciousOG</div>   <div className="prodigital">© 2024 Formosa Investments - All Rights Reserved</div> </div>
   
   
   
---
+++ app/components/FooterBounded.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
+import {Pontano_Sans} from "next/font/google";
+
+
+
+type BoundedProps = {
+as? : React.ElementType;
+className? : string;
+children : ReactNode;
+
+
+}
+
+const pontanoSans = Pontano_Sans({subsets: ["latin"],weight: "400"})
+export default function Bounded({
+as: Comp = "section",
+className,
+children,
+...restProps
+
+
+}:BoundedProps) {
+  return (
+    <Comp className={clsx("w-full  h-fit flex bg-[#162226] backdrop-filter backdrop-blur-md flex-col  items-center overflow-x-hidden  ",className)  } {...restProps} style={pontanoSans.style}>
+      <div className={clsx("wall   w-full portrait:w-[96%] h-full ",className)} {...restProps}  >{children}</div>
+    </Comp>
+  )
+}
