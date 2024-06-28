efcd42ead26b794de8bb0676a82593c58222dcf0
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-28 08:27:21 +0100

    [styling]:about page

--- slices/About/index.tsx
+++ slices/About/index.tsx
@@ -1,5 +1,6 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import Bounded from '../../app/components/Bounded';
 
 /**
  * Props for `About`.
@@ -11,12 +12,27 @@ export type AboutProps = SliceComponentProps<Content.AboutSlice>;
  */
 const About = ({ slice }: AboutProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for about (variation: {slice.variation}) Slices
-    </section>
+    <Bounded
+    data-slice-type={slice.slice_type}
+    data-slice-variation={slice.variation}
+
+    className="pt-10 pb-[10vw] portrait:pb-[20vw] rounded  text-[#333D3E] space-y-10  portrait:space-y-80 portrait:sm:space-y-40"
+  >
+   
+
+<div className="content w-full py-4 px-5 flex flex-col items-center space-y-[2.5vw] portrait:space-y-[4vw]">
+
+<div className="header text-[2vw] portrait:sm:text-[4vw] portrait:text-[7vw] text-center">{slice.primary.header}</div>
+
+<div className="writeup text-[1.4vw] portrait:sm:text-[3vw] portrait:text-[5vw]  text-center leading-[2.5vw] portrait:leading-[8vw] portrait:sm:leading-[5vw]">{slice.primary.writeup1}</div>
+<div className="writeup text-[1.4vw] portrait:sm:text-[3vw] portrait:text-[5vw]  text-center leading-[2.5vw] portrait:leading-[8vw] portrait:sm:leading-[5vw]">{slice.primary.writupe2}</div>
+<div className="writeup text-[1.4vw] portrait:sm:text-[3vw] portrait:text-[5vw]  text-center leading-[2.5vw] portrait:leading-[8vw] portrait:sm:leading-[5vw]">{slice.primary.writeup3}</div>
+
+</div>
+
+
+
+  </Bounded>
   );
 };
 
