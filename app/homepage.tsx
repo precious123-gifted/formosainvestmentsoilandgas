a5577db2387eb5794b79b5e7717f0361118defa2
precious123-gifted <ogunameprecious123@gmail.com>
2024-09-06 22:14:25 +0100

    restyle

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -1,14 +1,22 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
 import SmoothScroller from '../smoothscroll'
 
 
 export default function Homepage({Children}:any) {
 
+  useEffect(() => {
+    const handleRefresh = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
 
+    window.addEventListener('load', handleRefresh);
+
+    return () => {
+      window.removeEventListener('load', handleRefresh);
+    };
+  }, []);
 
  
 
